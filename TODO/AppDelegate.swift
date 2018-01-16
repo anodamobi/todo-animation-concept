219e36db2863f298576ccaac7567c3420905f68a
simon.kostenko <simon.kostenko@anoda.mobi>
2018-01-16 16:53:23 +0200

    Initial commit.

---
+++ TODO/AppDelegate.swift
@@ -0,0 +1,25 @@
+//
+//  AppDelegate.swift
+//  TODO
+//
+//  Created by Simon Kostenko on 1/16/18.
+//  Copyright © 2018 Simon Kostenko. All rights reserved.
+//
+
+import UIKit
+
+@UIApplicationMain
+class AppDelegate: UIResponder, UIApplicationDelegate {
+
+    var window: UIWindow?
+
+    func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplicationLaunchOptionsKey: Any]?) -> Bool {
+        
+        window = UIWindow(frame: UIScreen.main.bounds)
+        window?.makeKeyAndVisible()
+        window?.rootViewController = ViewController()
+        
+        return true
+    }
+}
+
