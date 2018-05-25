ebedf7851ab3f9bd761894e29c845aab5412ecae
Maxim Danilov <maxim.danilov@anoda.mobi>
2018-05-25 16:34:30 +0300

    Added 'Constants' file.

---
+++ TODO/Extensions/Constants.swift
@@ -0,0 +1,11 @@
+//
+//  Constants.swift
+//  TODO
+//
+//  Created by Maxim Danilov on 5/25/18.
+//  Copyright © 2018 ANODA. All rights reserved.
+//
+
+import Foundation
+
+typealias Localizable = R.string.localizable
