7d42df421ad901645291f512adac91c5f04a9d5d
simon.kostenko <simon.kostenko@anoda.mobi>
2018-06-15 23:40:21 +0300

    new animation implementation

--- TODO/UserStories/NewTask/NewTaskAnimator.swift
+++ TODO/UserStories/NewTask/NewTaskAnimator.swift
@@ -22,7 +22,7 @@ import UIKit
 
 class NewTaskAnimator: NSObject, UIViewControllerAnimatedTransitioning {
     
-    let duration = 0.6
+    let duration = 0.75
     var presenting = true
     private var keyboardRect: CGRect?
     
@@ -93,7 +93,7 @@ class NewTaskAnimator: NSObject, UIViewControllerAnimatedTransitioning {
         guard let toVC = transitionContext.viewController(forKey: .to) else { return }
         let finalFrame = transitionContext.finalFrame(for: toVC)
         newTaskView.frame = finalFrame
-        newTaskView.taskDetailsTextView.becomeFirstResponder()
+//        newTaskView.taskDetailsTextView.becomeFirstResponder()
         newTaskView.addNewTaskButton.isHidden = true
         let dismissView = self.dismissView()
         containerView.addSubview(dismissView)
@@ -101,9 +101,9 @@ class NewTaskAnimator: NSObject, UIViewControllerAnimatedTransitioning {
         newTaskView.alpha = 0.0
         newTaskView.center.y += 40.0
         
-        guard let projectView = transitionContext.view(forKey: .from) as? ProjectTasksView else { return }
-        projectView.newTaskButton.isHidden = true
-        let animatedAddTaskButton = animatedNewTaskButtonForPresenting(projectView: projectView)
+//        guard let projectView = transitionContext.view(forKey: .from) as? ProjectTasksView else { return }
+//        projectView.newTaskButton.isHidden = true
+        let animatedAddTaskButton = animatedNewTaskButtonForPresenting(newTaskView: newTaskView)
         containerView.addSubview(animatedAddTaskButton)
         UIView.animateKeyframes(withDuration: duration, delay: 0.0, animations: {
             UIView.addKeyframe(withRelativeStartTime: 0.0, relativeDuration: 0.75, animations: {
@@ -114,18 +114,38 @@ class NewTaskAnimator: NSObject, UIViewControllerAnimatedTransitioning {
                 newTaskView.center.y -= 40.0
             })
         }, completion: { _ in
-            newTaskView.addNewTaskButton.isHidden = false
-            projectView.newTaskButton.isHidden = false
-            animatedAddTaskButton.removeFromSuperview()
+//            newTaskView.taskDetailsTextView.becomeFirstResponder()
+            
+            
+//            projectView.newTaskButton.isHidden = false
+//            animatedAddTaskButton.removeFromSuperview()
             dismissView.removeFromSuperview()
-            transitionContext.completeTransition(true)
+//            newTaskView.addNewTaskButton.isHidden = false
+//            transitionContext.completeTransition(true)
         })
-        guard let keyboardRect = self.keyboardRect else { return }
-        let newFrame = CGRect(x: 0,
-                                   y: keyboardRect.origin.y - animatedAddTaskButton.frame.height,
-                                   width: UIScreen.main.bounds.width,
-                                   height: animatedAddTaskButton.frame.height)
-        animateNewTaskButton(animatedAddTaskButton, to: newFrame)
+        
+        DispatchQueue.main.asyncAfter(deadline: .now() + duration / 2) {
+            newTaskView.taskDetailsTextView.becomeFirstResponder()
+            UIView.animate(withDuration: 0.35, delay: 0.0, options: UIViewAnimationOptions(rawValue: 7), animations: {
+                animatedAddTaskButton.frame.origin.y = (self.keyboardRect?.origin.y)! - animatedAddTaskButton.frame.height
+            }, completion: { _ in
+                newTaskView.addNewTaskButton.isHidden = false
+                transitionContext.completeTransition(true)
+            })
+        }
+        
+        
+//        UIView.animate(withDuration: 0.3, delay: duration * 0.8, animations: {
+//            newTaskView.addNewTaskButton.alpha = 1.0
+//        }) { _ in
+//            transitionContext.completeTransition(true)
+//        }
+//        guard let keyboardRect = self.keyboardRect else { return }
+//        let newFrame = CGRect(x: 0,
+//                                   y: 300,//keyboardRect.origin.y - animatedAddTaskButton.frame.height,
+//                                   width: UIScreen.main.bounds.width,
+//                                   height: animatedAddTaskButton.frame.height)
+//        animateNewTaskButton(animatedAddTaskButton, to: newFrame)
     }
 
     // MARK: - Private Methods
@@ -138,14 +158,13 @@ class NewTaskAnimator: NSObject, UIViewControllerAnimatedTransitioning {
         animateCornerRadius(for: animatedAddTaskButton)
     }
     
-    private func animatedNewTaskButtonForPresenting(projectView: ProjectTasksView) -> UIButton {
+    private func animatedNewTaskButtonForPresenting(newTaskView: NewTaskView) -> UIButton {
         let animatedAddTaskButton = UIButton()
-        animatedAddTaskButton.frame = projectView.newTaskButton.frame
-        animatedAddTaskButton.clipsToBounds = true
-        animatedAddTaskButton.layer.cornerRadius = animatedAddTaskButton.frame.width / 2.0
-        animatedAddTaskButton.backgroundColor = projectView.newTaskButton.backgroundColor
-        animatedAddTaskButton.setTitle(projectView.newTaskButton.titleLabel?.text, for: .normal)
-        animatedAddTaskButton.setTitleColor(UIColor.white, for: .normal)
+        animatedAddTaskButton.frame = CGRect(x: 0, y: UIScreen.main.bounds.height, width: UIScreen.main.bounds.width, height: 54.0)//newTaskView.addNewTaskButton.frame
+//        animatedAddTaskButton.frame.origin.y = UIScreen.main.bounds.height
+        animatedAddTaskButton.backgroundColor = newTaskView.addNewTaskButton.backgroundColor
+        animatedAddTaskButton.setTitle(newTaskView.addNewTaskButton.titleLabel?.text, for: .normal)
+        animatedAddTaskButton.setTitleColor(newTaskView.addNewTaskButton.titleColor(for: .normal), for: .normal)
         animatedAddTaskButton.titleLabel?.font = .romanTitle
         return animatedAddTaskButton
     }
